b5e60333da1c66321f3149f01e4248933f941c1a
Gustavo Fassoni Shinkai <gufashi2006@gmail.com>
2025-06-13 21:48:42 -0300

    AULA 04

---
+++ Eclipse/br.com.senaisp.bauru.aula03/src/br/com/senaisp/bauru/aula04/Exemplo02.java
@@ -0,0 +1,24 @@
+package br.com.senaisp.bauru.aula04;
+
+public class Exemplo02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          String str01 = "Roberto Da Silva Sauro";
+          //verificar se há dino no texto
+         System.out.println(str01.contains("Sauro"));
+         //Obter a posição no texto de um subtexto
+         System.out.println(str01.indexOf("o",3));
+         //Obter o caracter na posição desejada
+         System.out.println(str01.charAt(3));
+         //Obter Substring de uma string sem limitar o final
+         System.out.println(stre01.substring(8));
+         //Obter Substring de uma string limitando o final
+         System.out.println(stre01.substring(8,16));
+         //Usando o compareTo
+         System.out.println(stre.compareTo("Roberval"));
+         
+        		 
+	}
+
+}
---
+++ Eclipse/br.com.senaisp.bauru.aula03/src/br/com/senaisp/bauru/aula04/Exercicio01.java
@@ -0,0 +1,21 @@
+package br.com.senaisp.bauru.aula04;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("DIgite seu nome completo: ");
+		String nome = sc.nextLine();
+		//Exemplo: Roberto Da Silva Sauro
+		//obter o primeiro nome e o ultimo nome que foi digitado
+		String priNome = nome.substring(0,nome.indexOf(" "));
+		String ultNome = nome.substring(nome.lastIndexOf(" ")+1);
+		
+		System.out.println(priNome);
+		System.out.println(ultNome);
+
+	}
+
+}
